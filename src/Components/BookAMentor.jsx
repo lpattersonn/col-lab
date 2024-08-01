463ea6df2bbed6adbf7d28c5d2061edb992de04c
Leshan <leshan0627@gmail.com>
2024-08-01 09:15:00 -0400

    feat: Add BookAMentor component

--- src/Components/BookAMentor.jsx
+++ src/Components/BookAMentor.jsx
@@ -1,8 +1,46 @@
 import React, {useState} from "react";
+import axios from "axios";
 
 export default function BookAMentor(prop) {
-const [ calenderModal, setCalenderModal ] = useState('hide');
-const [overFLow, setOverFlow] = useState(false);
+const userDetails = JSON.parse(localStorage.getItem('userDetails'));
+const [mentorRequest, setMentorRequest] = useState({
+    mentor_request_date: '',
+    mentor_request_time: '',
+    mentor_request_hours: '',
+    mentor_request_notes: ''
+})
+
+const handleSubmit = (e) => {
+    e.preventDefault();
+    axios.get(`${process.env.REACT_APP_API_URL}/wp-json/wp/v2/mentor-requests/`,
+        {   
+            'acf' : {
+                'mentor_request_date': mentorRequest.mentor_request_date,
+                'mentor_request_time:': mentorRequest.mentor_request_time,
+                'mentor_request_hours': mentorRequest.mentor_request_hours,
+                'mentor_request_notes': mentorRequest.mentor_request_notes,
+            }
+        },
+        {
+            headers: {
+                Authorization: `Bearer ${userDetails.token}`
+            }
+        }
+    ).then(function(response) {
+        console.log(response);
+    })
+}
+
+
+//   Handle Change
+function handleChange(e) {
+    const {name, value} = e.target
+    setMentorRequest(prev => {
+        return (
+            { ...prev, [name]: value}
+        )
+    })
+}
 
 return (
     <div className={`modal-container ${prop.prop1}`}>
@@ -10,29 +48,35 @@ return (
         <div className={"calender-schedule-modal"+' '+"p-3"+' '+`${prop.prop1}`}>
             <div className='card'>
                 <div className='card-body'>
-                    <form>
+                    <form onSubmit={handleSubmit}>
                         <h4>Mentor Request Form</h4>
                         <div className='row mb-3'>
                             <div className='col-12'>
-                                <label className='small m-0'>Please indicate your proposed meeting date/time</label>
-                                <input className="form-control" type="date" aria-label="event-date" />
+                                <label for="mentor_request_date" className='small m-0'>Please indicate your proposed meeting date</label>
+                                <input className="form-control" value={mentorRequest.mentor_request_date} name="mentor_request_date" type="date" aria-label="event-date" onChange={handleChange} />
                             </div>
                         </div>
                         <div className='row mb-3'>
                             <div className='col-12'>
-                                <label className='small m-0'>Number of hours</label>
-                                <input className="form-control" type="number" aria-label="event-duration" />
+                                <label for="mentor_request_time" className='small m-0'>Please indicate your proposed meeting time</label>
+                                <input className="form-control" value={mentorRequest.mentor_request_time} name="mentor_request_time" type="time" aria-label="event-time" onChange={handleChange} />
                             </div>
                         </div>
                         <div className='row mb-3'>
                             <div className='col-12'>
-                                <label className='small m-0'>Notes</label>
-                                <textarea row="3" className="form-control" aria-label="event-message" />
+                                <label for="mentor_request_hours" className='small m-0'>Number of hours</label>
+                                <input className="form-control" value={mentorRequest.mentor_request_hours} name="mentor_request_hours" type="number" aria-label="event-duration" onChange={handleChange} />
+                            </div>
+                        </div>
+                        <div className='row mb-3'>
+                            <div className='col-12'>
+                                <label for="mentor_request_notes" className='small m-0'>Notes</label>
+                                <textarea row="3" value={mentorRequest.mentor_request_notes} className="form-control" name="mentor_request_notes" aria-label="event-message" onChange={handleChange} />
                             </div>
                         </div>
                         <div className='row'>
                             <div className='col-12'>
-                                <input className='btn btn-primary' type="submit" aria-label="submit" />
+                                <input className='btn btn-primary' name="" type="submit" aria-label="submit" />
                             </div>
                         </div>
                     </form>
