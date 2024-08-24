35a3cbaead6175d11e7c07da2a04e6fc77e623f5
Leshan <leshan0627@gmail.com>
2024-08-24 00:20:15 -0400

    feat: update requests

--- src/Components/MentorChat.jsx
+++ src/Components/MentorChat.jsx
@@ -52,7 +52,7 @@ export default function MentorChat() {
             })
             setRequest(objects?.length)
         }).catch((err) => {})
-    }, [calenderModal]);
+    }, );
 
     // Set user information
     useEffect(() => {
