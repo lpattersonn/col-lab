5695d1f99fce4330a882b083cf8afd3310bc9580
Leshan Patterson <leshan@leshanpatterson.com>
2025-04-10 01:12:17 -0400

    feat: Update archive functionality for single archive page

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import './Styles/_compiled/style.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'mdb-ui-kit'; // ← This is often all you need
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
